559f3db68d642a4260e6a7746e2d6fe4dd5ee8da
riku0202 <riku0202excalibur@gmail.com>
2022-08-02 07:30:19 +0900

    feat: add algo

--- go.mod
+++ go.mod
@@ -2,6 +2,11 @@ module github.com/riku0202/bitbank-system-trading
 
 go 1.18
 
-require gopkg.in/ini.v1 v1.66.6
+require (
+	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70
+	github.com/mattn/go-sqlite3 v1.14.14
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
+	gopkg.in/ini.v1 v1.66.6
+)
 
 require github.com/stretchr/testify v1.8.0 // indirect
--- utils/logging.go
+++ utils/logging.go
@@ -6,12 +6,12 @@ import (
 	"os"
 )
 
-func LoggingSetting(logFile string) {
-	lf, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+func LoggingSettings(logFile string) {
+	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("file=logFile err=%s", err.Error())
 	}
-	multiLogFile := io.MultiWriter(os.Stdout, lf)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.SetOutput(multiLogFile)
 }
